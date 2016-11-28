94842b69733a55b586f1a0bd77de8ce933eca508
aturcey <adrienturcey@outlook.com>
2016-11-28 19:44:33 +0100

    Initialize Product and ShoppingList models

---
+++ app/src/main/java/com/clevermind/shoppinglist/model/ShoppingList.java
@@ -0,0 +1,52 @@
+package com.clevermind.shoppinglist.model;
+
+import java.util.Date;
+
+/**
+ * Created by adrien on 28/11/16.
+ */
+
+public class ShoppingList {
+
+    private Integer id;
+
+    private String name;
+
+    private Date createdDate;
+
+    private Boolean completed;
+
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Date getCreatedDate() {
+        return createdDate;
+    }
+
+    public void setCreatedDate(Date createdDate) {
+        this.createdDate = createdDate;
+    }
+
+    public Boolean getCompleted() {
+        return completed;
+    }
+
+    public void setCompleted(Boolean completed) {
+        this.completed = completed;
+    }
+
+}
